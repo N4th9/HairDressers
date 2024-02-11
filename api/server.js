17f65b63610e263865c861a25c6a458a31257566
N4th9 <128613887+N4th9@users.noreply.github.com>
2024-02-11 18:20:29 +0100

    Map affichée -> Page de connexion

--- api/server.js
+++ api/server.js
@@ -11,14 +11,16 @@ const db = new sqlite3.Database('api/data/HairDressers.db');
 app.get('/api/HairDressers', (req, res) => {
     const OFFSET = req.query.offset || 0;
     const LIMIT = 8;
-    db.all('SELECT id, nom, voie, ville, code_postal FROM coiffeurs LIMIT ? OFFSET ?', [LIMIT, OFFSET], (err, rows) => {
+    db.all('SELECT id, nom, numero, voie, ville, code_postal, latitude, longitude FROM coiffeurs LIMIT ? OFFSET ?', [LIMIT, OFFSET], (err, rows) => {
         res.send(rows);
         console.log("Appel de 7 nouveaux coiffeurs");
     });
 });
 app.get('/api/searchHairdressers', (req, res) => {
     const term = req.query.term;
-    db.all('SELECT id, nom, voie, ville, code_postal FROM coiffeurs WHERE nom LIKE ?', [`%${term}%`], (err, rows) => {
+    const OFFSET = parseInt(req.query.offset) || 0;
+    const LIMIT = 8;
+    db.all('SELECT id, nom, numero, voie, ville, code_postal, latitude, longitude FROM coiffeurs WHERE nom LIKE ? LIMIT ? OFFSET ?', [`%${term}%`, LIMIT, OFFSET], (err, rows) => {
         if (err) {
             console.error('Erreur lors de la recherche de coiffeurs dans la base de données :', err);
             res.status(500).send('Erreur lors de la recherche de coiffeurs dans la base de données');
--- public/script.js
+++ public/script.js
@@ -1,16 +1,17 @@
 const localhost = 'http://localhost:3000';
 const limit = 8;
 let offset = 0;
-let totalHairDressers = 0;
 
-let hairDressers = [
+let MyActualHairDresser = [
     {
         id: 0,
-        NameHairDresser: "",
-        NumberHairDresser: "",
-        StreetHairDresser: "",
-        CodePostalHairDresser: "",
-        CityHairDresser: "",
+        Name: "",
+        Number: "",
+        Rue: "",
+        PostalCode: "",
+        City: "",
+        Latitude: "",
+        Longitude: ""
     }
 ];
 
@@ -20,116 +21,149 @@ document.addEventListener('DOMContentLoaded', function () {
     const LoadHairDressers = document.getElementById('LoadHairDressers');
     const SearchHairDressers = document.getElementById('SearchHairDressers');
     const BackList = document.getElementById('BackList');
+    const NomHairDresser = document.getElementById("NomHairDresser")
+    const NumeroHairDresser = document.getElementById("NumeroHairDresser")
+    const VoieHairDresser = document.getElementById("VoieHairDresser")
+    const CodePostalHairDresser = document.getElementById("CodePostalHairDresser")
+    const VilleHairDresser = document.getElementById("VilleHairDresser")
+    const MapContainerLocation = document.getElementById("MapContainerLocation")
 
     DisplayHairDressers();
 
     LoadHairDressers.addEventListener('click', LoadButton);
     SearchHairDressers.addEventListener('keyup', SearchButton);
 
+    function LoadInfosHairDresser(DivHairDresser, data, i) {
+        console.log(data[i])
+        if (DivHairDresser.classList.contains("Purple")) {
+            pageListSearch.classList.remove("details");
+            DivHairDresser.classList.remove("Purple");
+            SearchHairDressers.classList.remove("searchDetails")
+        } else {
+            const allHairDressers = document.querySelectorAll('.DivHairDresser');
+            allHairDressers.forEach(hairDresser => {
+                hairDresser.classList.remove("Purple");
+            });
+            pageListSearch.classList.add("details");
+            BackList.classList.add("details");
+            DivHairDresser.classList.add("Purple");
+            SearchHairDressers.classList.add("searchDetails")
+            console.log(data[i]);
+        }
+        MyActualHairDresser.id = data[i].id;
+        MyActualHairDresser.name = data[i].nom;
+        MyActualHairDresser.Number = data[i].numero;
+        MyActualHairDresser.Rue = data[i].voie;
+        MyActualHairDresser.PostalCode = data[i].code_postal;
+        MyActualHairDresser.City = data[i].ville;
+
+        NomHairDresser.innerText = MyActualHairDresser.name;
+        NumeroHairDresser.innerText = MyActualHairDresser.Number;
+        VoieHairDresser.innerText = MyActualHairDresser.Rue;
+        CodePostalHairDresser.innerText = MyActualHairDresser.PostalCode;
+        VilleHairDresser.innerText = MyActualHairDresser.City;
+
+        MyActualHairDresser.Latitude = data[i].latitude;
+        MyActualHairDresser.Longitude = data[i].longitude;
+        let latitude = MyActualHairDresser.Latitude;
+        let longitude = MyActualHairDresser.Longitude;
+
+        if (MyActualHairDresser.Latitude !== undefined && MyActualHairDresser.Longitude !== undefined) {
+            const mapContainer = document.createElement("div");
+            mapContainer.id = 'map';
+            mapContainer.classList.add("maps");
+            MapContainerLocation.innerHTML = "";
+            MapContainerLocation.appendChild(mapContainer);
+            const MyMap = L.map(mapContainer).setView([latitude, longitude], 15);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© OpenStreetMap contributors'
+            }).addTo(MyMap);
+
+            L.marker([latitude, longitude]).addTo(MyMap)
+                .bindPopup('Emplacement')
+                .openPopup();
+        } else {
+            console.error("Latitude ou longitude manquante pour le coiffeur :", data[i]);
+        }
+    }
+    function DataHairDressers(data) {
+        if (data.length > 0) {
+            for (let i = 0; i < data.length; i++) {
+                let DivHairDresser = document.createElement('div');
+                DivHairDresser.classList.add('DivHairDresser');
+
+                DivHairDresser.addEventListener('click', function () {
+                    LoadInfosHairDresser(DivHairDresser, data, i);
+                });
+
+                let DivInfos = document.createElement('div');
+                DivInfos.classList.add('DivInfos');
+
+                let DivIndex = document.createElement('div');
+                DivIndex.classList.add('DivIndex');
+
+                let pTitle = document.createElement('p');
+                pTitle.innerHTML = `<p>${data[i].nom}</p>`;
+
+                let pVoie = document.createElement('p');
+                pVoie.innerHTML = `<p>${data[i].numero} ${data[i].voie}</p>`;
+
+                let pCodeVille = document.createElement('p');
+                pCodeVille.innerHTML = `<p>${data[i].code_postal} ${data[i].ville}</p>`;
+
+                let pIndex = document.createElement('p');
+                pIndex.innerHTML = `<p class="pIndex">${data[i].id}</p>`;
+                pIndex.classList.add('pIndex');
+
+                DivHairDresser.appendChild(DivInfos);
+                DivHairDresser.appendChild(DivIndex);
+                DivInfos.appendChild(pTitle);
+                DivInfos.appendChild(pVoie);
+                DivInfos.appendChild(pCodeVille);
+                DivIndex.appendChild(pIndex);
+                ListHairDressers.appendChild(DivHairDresser); // Ajout à la liste existante
+            }
+        }
+    }
     function LoadButton() {
         offset += limit;
-        fetchData(offset);
+        if (SearchHairDressers.value === '') {
+            fetchData(offset);
+        } else {
+            SearchButton(offset);
+        }
     }
-
-    function SearchButton() {
+    function SearchButton(offset) {
         const searchQuery = SearchHairDressers.value;
-        ListHairDressers.innerHTML = '';
-        fetch(`/api/searchHairdressers?term=${searchQuery}`)
+        fetch(`/api/searchHairdressers?term=${searchQuery}&offset=${offset}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                for (let i = 0; i < data.length; i++) {
-                    let DivHairDresser = document.createElement('div');
-                    DivHairDresser.classList.add('DivHairDresser');
-
-                    let DivInfos = document.createElement('div');
-                    DivInfos.classList.add('DivInfos');
-
-                    let DivIndex = document.createElement('div');
-                    DivIndex.classList.add('DivIndex');
-
-                    let pTitle = document.createElement('p');
-                    pTitle.innerHTML = `<p>${data[i].nom}</p>`;
-
-                    let pVoie = document.createElement('p');
-                    pVoie.innerHTML = `<p>${data[i].voie}</p>`;
-
-                    let pCodeVille = document.createElement('p');
-                    pCodeVille.innerHTML = `<p>${data[i].code_postal} ${data[i].ville}</p>`;
-
-                    let pIndex = document.createElement('p');
-                    pIndex.innerHTML = `<p class="pIndex">${data[i].id}</p>`;
-                    pIndex.classList.add('pIndex');
-
-                    ListHairDressers.appendChild(DivHairDresser);
-                    DivHairDresser.appendChild(DivInfos);
-                    DivHairDresser.appendChild(DivIndex);
-                    DivInfos.appendChild(pTitle);
-                    DivInfos.appendChild(pVoie);
-                    DivInfos.appendChild(pCodeVille);
-                    DivIndex.appendChild(pIndex);
-                }
+                ListHairDressers.innerHTML = '';
+                DataHairDressers(data);
             })
             .catch(error => {
                 console.error('Erreur lors de la recherche de coiffeurs :', error);
             });
     }
-
-
     function DisplayHairDressers() {
         fetchData(offset);
-
         window.addEventListener('scroll', function () {
             if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
-                offset += limit;
-                fetchData(offset);
+                if (SearchHairDressers.value === '') {
+                    offset += limit;
+                    fetchData(offset);
+                } else if (SearchHairDressers.value !== '') {
+                    offset += limit;
+                    SearchButton(offset);
+                }
             }
         });
     }
-
     function fetchData(offset) {
         fetch(localhost + `/api/HairDressers?offset=${offset}`)
             .then(response => response.json())
             .then(function (data) {
-                if (data.length > 0) {
-                    for (let i = 0; i < data.length; i++) {
-                        totalHairDressers = totalHairDressers + 8;
-                        let DivHairDresser = document.createElement('div');
-                        DivHairDresser.classList.add('DivHairDresser');
-
-                        DivHairDresser.addEventListener('click', function () {
-                            pageListSearch.classList.add("details");
-                            BackList.classList.add("details");
-                        });
-
-                        let DivInfos = document.createElement('div');
-                        DivInfos.classList.add('DivInfos');
-
-                        let DivIndex = document.createElement('div');
-                        DivIndex.classList.add('DivIndex');
-
-                        let pTitle = document.createElement('p');
-                        pTitle.innerHTML = `<p>${data[i].nom}</p>`;
-
-                        let pVoie = document.createElement('p');
-                        pVoie.innerHTML = `<p>${data[i].voie}</p>`;
-
-                        let pCodeVille = document.createElement('p');
-                        pCodeVille.innerHTML = `<p>${data[i].code_postal} ${data[i].ville}</p>`;
-
-                        let pIndex = document.createElement('p');
-                        pIndex.innerHTML = `<p class="pIndex">${data[i].id}</p>`;
-                        pIndex.classList.add('pIndex');
-
-                        ListHairDressers.appendChild(DivHairDresser);
-                        DivHairDresser.appendChild(DivInfos);
-                        DivHairDresser.appendChild(DivIndex);
-                        DivInfos.appendChild(pTitle);
-                        DivInfos.appendChild(pVoie);
-                        DivInfos.appendChild(pCodeVille);
-                        DivIndex.appendChild(pIndex);
-                    }
-                }
+                DataHairDressers(data);
             });
     }
 });
